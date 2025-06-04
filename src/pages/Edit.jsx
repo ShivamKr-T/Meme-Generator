6bfa41d1952f5fd294b2eea034b4b15566f5ca5b
ShivamKr-T <kumarshivam63834@gmail.com>
2025-06-04 10:59:07 +0530

    Added Save feature

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,22 +1,40 @@
-import React,{useState} from "react";
+import React,{useState,useRef} from "react";
 import Text from "../components/Text";
 import { useSearchParams } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import * as htmlToImage from 'html-to-image';
+import download from 'downloadjs';
 
 const EditPage=()=>{
     const [params]=useSearchParams();
     const [count,setCount]=useState(0);
 
+    const memeRef=useRef(null);
+
     const addText=()=>{
         setCount(count+1);
     };
 
+    const downloadMeme = () => {
+        if (!memeRef.current) return;
+
+        htmlToImage.toJpeg(memeRef.current, { quality: 0.95 })
+        .then((dataUrl) => {
+            download(dataUrl, "meme.jpeg");
+        })
+        .catch((err) => {
+            console.error("Export failed:", err);
+        });
+    };
+
     return(
         <div>
-            <div>
+            <div style={{width:'500px',border:'1px solid',backgroundColor:'white'}} ref={memeRef} className="meme mt-5 mb-5">
                 <img src={params.get("url")} width="250px" />
                 {Array(count).fill(0).map((e)=>(<Text/>))}
             </div>
-            <button onClick={addText}>Add Text</button>
+            <Button onClick={addText} style={{marginRight:'5px'}}>Add Text</Button>
+            <Button variant="success" onClick={downloadMeme}>Save</Button>
         </div>
     )
 }
